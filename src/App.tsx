5f7cf536e1e684682765573a194e153145149bab
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-02-19 14:37:36 +0530

    navbar creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-
 import './App.css'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-function App() {
-  
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
+function App() {
   return (
- <>
-  <Button colorScheme='blue'>Button</Button>
- </>
-    
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`
+        }}
+       // gap={4} // Add gap between grid items
+      >
+        <GridItem area='nav' bg='coral'>Nav</GridItem>
+        <Show above ='lg'>
+        <GridItem area='aside' bg='gold'>Aside</GridItem> {/* Change area name to 'aside' */}
+        </Show>
+        <GridItem area='main' bg='dodgerblue'>Main</GridItem> {/* Change area name to 'main' */}
+      </Grid>
+    </>
   )
 }
 
