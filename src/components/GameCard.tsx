5d28626b72eb488d6ac54e36c784c2b6b9bb3342
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-02-20 17:40:24 +0530

    reafctor remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,7 +13,7 @@ const GameCard = ({game}:Props) => {
        console.log(getCroppedImageUrl(game.background_image));
        
   return (
-  <Card borderRadius={10} overflow='hidden'>
+  <Card>
        <Image src= {getCroppedImageUrl(game.background_image)}/>
        <CardBody>
               <Heading fontSize='2xl'>{game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,25 +2,37 @@ import React, { useEffect, useState } from 'react'
 import apiClient from '../services/api-client'
 import axios from 'axios';
 import useGames from '../hooks/useGames';
-import { SimpleGrid,Text } from '@chakra-ui/react';
+import { SimpleGrid, Text } from '@chakra-ui/react';
 import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContanier from './GameCardContanier';
 
 
 
 
 const GameGrid = () => {
-    const {games,error} = useGames()    
+       const { games, error, isLoading } = useGames()
+       const Skeletons = [1, 2, 3, 4, 5, 6];
+       return (
+              <>
+                     {error && <Text>{error}</Text>}
+                     <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding={10} spacing={10}>
+                            {isLoading && Skeletons.map(Skeleton => (
+                            <GameCardContanier>
+                            <GameCardSkeleton key={Skeleton} />
+                            </GameCardContanier>
+                            ))}
+                            {games.map(game => (
+                            
+                            <GameCardContanier>
+                                   <GameCard key={game.id} game={game} />
+                            </GameCardContanier>
+                            
+                            ))}
 
-  return (
-    <>
-       {error&& <Text>{error}</Text>}
-       <SimpleGrid columns={{sm:1,md:2,lg:3,xl:5}} padding={10} spacing={10}>
-               {games.map(game=> <GameCard key={game.id} game={game}/>
-              )}
-
-       </SimpleGrid>
-    </>
-  )
+                     </SimpleGrid>
+              </>
+       )
 }
 
 export default GameGrid
